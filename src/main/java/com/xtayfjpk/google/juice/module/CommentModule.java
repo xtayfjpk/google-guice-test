90abf912a97d0424cdb0f90a5a11d1d2eb605cc2
zhangjin <zhangjin@kingyea.com.cn>
2014-10-31 21:28:41 +0800

    添加@Provides Methods测试

--- src/main/java/com/xtayfjpk/google/juice/module/CommentModule.java
+++ src/main/java/com/xtayfjpk/google/juice/module/CommentModule.java
@@ -1,6 +1,7 @@
 package com.xtayfjpk.google.juice.module;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 import com.google.inject.name.Names;
 import com.xtayfjpk.google.juice.dao.CommentDao;
 import com.xtayfjpk.google.juice.dao.CommentDaoImpl;
@@ -11,9 +12,14 @@ public class CommentModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
-		this.bind(CommentDao.class).to(CommentDaoImpl.class);
+		//this.bind(CommentDao.class).to(CommentDaoImpl.class);
 		this.bind(CommentService.class).to(CommentServiceImpl.class);
 		this.bind(String.class).annotatedWith(Names.named("value")).toInstance("hello guice");
 	}
 
+	
+	@Provides
+	public CommentDao provideCommentDao() {
+		return new CommentDaoImpl();
+	}
 }
