df7a79ee733844c54eeb1b984b8816d44ef7b545
zhangjin <zhangjin@kingyea.com.cn>
2014-10-31 21:37:18 +0800

    添加对Constructor Bindings测试

--- src/main/java/com/xtayfjpk/google/juice/module/BillingModule.java
+++ src/main/java/com/xtayfjpk/google/juice/module/BillingModule.java
@@ -17,7 +17,14 @@ public class BillingModule extends AbstractModule {
 	protected void configure() {
 		this.bind(BillingService.class).to(RealBillingService.class);
 		//this.bind(TransactionLog.class).to(DatabaseTransactionLog.class);
-		this.bind(TransactionLog.class).toProvider(DatabaseTransactionLogProvider.class);
+		//this.bind(TransactionLog.class).toProvider(DatabaseTransactionLogProvider.class);
+		try {
+			this.bind(TransactionLog.class).toConstructor(DatabaseTransactionLog.class.getConstructor());
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		}
 		this.bind(CreditCardProcessor.class).annotatedWith(Repository.class)
 		.to(PayPalCreditCardProcessor.class);
 		this.bind(String.class).annotatedWith(Names.named("value")).toInstance("hello guice");
